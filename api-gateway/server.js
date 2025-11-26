491d97dc3c3aef58991953f87607ab410bf06fc2
Shashank <achryashashank@gmail.com>
2025-11-26 17:22:55 +0530

    api-gateway cors to https://whats-my-dns-microservice-9eew.vercel.app/

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(express.json());
 
 app.use(cors({
-  origin:["http://localhost:5173","https://whats-my-dns-client.vercel.app"],
+  origin:["http://localhost:5173","https://whats-my-dns-client.vercel.app","https://whats-my-dns-microservice-9eew.vercel.app/"],
   methods:["GET","POST", "OPTIONS"],
   credentials:true
 }));
